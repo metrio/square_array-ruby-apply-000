145aeac6300782645c3500de88edc86d5092ed7e
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-24 16:05:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   square_array = (array)
   square_array.each do |numbers|
-  square_array << numbers ** 2
-   return square_array
+  new_array << numbers ** 2
+  
   # your code here
   end
+  return new_array
 end
